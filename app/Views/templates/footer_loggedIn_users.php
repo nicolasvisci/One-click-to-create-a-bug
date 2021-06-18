7c3596bc0e9b3099fa022b2bc64298dd4316123a
MatteoMassaro <84584503+MatteoMassaro@users.noreply.github.com>
2021-06-18 16:29:31 +0200

    Delete app directory

--- app/Views/templates/footer_loggedIn_users.php
+++
@@ -1,8 +0,0 @@
-        <footer>
-            <nav>
-                <button type="submit" name="prenota" class="material-icons" id="prenota"><a class="icons">bookmark_add</a></button>
-                <button type="submit" name="history" class="material-icons" id="history"><a class="icons">view_list</a></button>
-            </nav>
-        </footer>
-    </body>
-</html>
