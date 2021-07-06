ee398be6b394e999ae4148d5eed46c6a875d3ef4
MatteoMassaro <massaromatteo21@gmail.com>
2021-07-06 19:55:21 +0200

    up

--- public/script.js
+++ public/script.js
@@ -2,20 +2,12 @@ $(document).ready(function() {
 
     var calendar = $('#calendar').fullCalendar({
         editable: true,
-        events: site_url + "/event",
         displayEventTime: false,
         editable: true,
         header:{
             left:'prev,next today',
             center:'title',
             right:'month,agendaWeek,agendaDay'
-        },
-        eventRender: function(event, element, view) {
-            if (event.allDay === 'true') {
-                event.allDay = true;
-            } else {
-                event.allDay = false;
-            }
         },
         selectable: true,
         selectHelper: true,
